d3f4469ea5ec026af0588b5d9bc95869c369fa26
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-30 08:26:09 +0530

    Routes

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import {Route, Routes} from 'react-router-dom'
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Contacts from '../pages/Contacts'
+import Doctors from '../pages/doctors/Doctors'
+import DoctorDetails from '../pages/doctors/DoctorDetails'
 
 const Routers = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:id' element={<DoctorDetails />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Signup />} />
+      <Route path='/contact' element={<Contacts />} />      
+    </Routes>
   )
 }
 

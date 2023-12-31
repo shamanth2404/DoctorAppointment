a916899939e3f5cdbbaae84c56e19c1d0b6b88f7
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-31 08:41:30 +0530

    Doctors List

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,10 @@ import {Link} from 'react-router-dom';
 import {BiRightArrow} from 'react-icons/bi';
 import About from "../components/about/About";
 import ServiceList from "../components/services/ServiceList";
+import featureImg from '../assets/images/feature-img.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
+import DoctorList from "../components/Doctors/DoctorList";
 const Home = () => {
   return (
     <>
@@ -150,6 +154,73 @@ const Home = () => {
           <ServiceList />
         </div>
       </section>
+      {/* Feature Section */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* Feature Content */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime
+              </h2>
+              <ul className="pl-4">
+                <li className="textPara">
+                  1.Schedule the appointment directly.
+                </li>
+                <li className="textPara">
+                  2.Search for your physician here,and contact their office.
+                </li>
+                <li className="textPara">3. View our physicians who are accepting new patients,use the online scheduling tool to select an appointment time.</li>
+              </ul>
+              <Link to='/'><button className="btn">Learn More</button></Link>
+            </div>
+            {/* Feature Image */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt:0">
+              <img src={featureImg} alt="" />
+
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3 text-[10px] leading-[10px] lg:text-[14px] lg:leading-5">
+                    <p className="text-headingColor font-[600]">
+                        Tue,24
+                    </p>
+                    <p className="text-textColor font-[400]">
+                      10:00AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lf:py-3 lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className="w-[65px] lg:w-[96px] bg-[#ccf0f3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
+
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Wayne Collins
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Our Great Doctors */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our great doctors</h2>
+            <p className="textPara text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquid dolorem ad consectetur, officiis asperiores!</p>
+          </div>
+          <DoctorList />
+        </div>
+      </section>
+      {/* FAQ Section */}
+      
     </>
   );
 };

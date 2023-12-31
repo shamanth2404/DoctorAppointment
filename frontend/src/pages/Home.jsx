7e45cabac06c363b27fb7dd31fe7885726b725e7
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-31 11:12:59 +0530

    FAQ Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,8 @@ import featureImg from '../assets/images/feature-img.png'
 import videoIcon from '../assets/images/video-icon.png'
 import avatarIcon from '../assets/images/avatar-icon.png'
 import DoctorList from "../components/Doctors/DoctorList";
+import faqImg from '../assets/images/faq-img.png'
+import FaqList from "../components/faq/FaqList";
 const Home = () => {
   return (
     <>
@@ -220,6 +222,23 @@ const Home = () => {
         </div>
       </section>
       {/* FAQ Section */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most Questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Testimonial */}
       
     </>
   );
